b89e2fd7f74cb302c27cbd01818c9f2628baf564
Bill <207559339@qq.com>
2019-09-20 22:39:13 +0800

    some changes

--- script.js
+++ script.js
@@ -201,11 +201,11 @@ var init = function(){
   var isSmall = window.innerWidth < 1000;
   
   var ps = new ParticleSlider({
-    ptlGap: isMobile || isSmall ? 3 : 0,
-    ptlSize: isMobile || isSmall ? 2 : 1,
+    ptlGap: isMobile || isSmall ? 0 : 0,
+    ptlSize: isMobile || isSmall ? 1 : 1,
     width: 1e9,
     height: 1e9,
-    mouseForce: 700
+    mouseForce: 100
   });
   
   (window.addEventListener
